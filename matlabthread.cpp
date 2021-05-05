6b461c0c23adfb417f1f72f0f10ae0c29cb3ac46
Matthew Mueller <mrm.mue@gmail.com>
2021-05-05 12:14:45 -0700

    Fixed exec path for the matlab thread

--- matlabthread.cpp
+++ matlabthread.cpp
@@ -16,7 +16,8 @@ matlabThread::~matlabThread(){
 void matlabThread::run(){
     // Start matlab and add needed paths
     matlabPtr = startMATLAB();
-    matlab::data::CharArray newDir = factory.createCharArray(QDir::currentPath().toStdString()+"/XR_Repository");
+    //matlab::data::CharArray newDir = factory.createCharArray(QDir::currentPath().toStdString()+"/XR_Repository");
+    matlab::data::CharArray newDir = factory.createCharArray(QCoreApplication::applicationDirPath().toStdString()+"/XR_Repository");
     matlabPtr->feval(u"cd",newDir);
     matlab::data::CharArray newGen = matlabPtr->feval(u"genpath",newDir);
     matlabPtr->feval(u"addpath",newGen);
