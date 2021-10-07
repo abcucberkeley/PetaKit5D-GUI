84eb77085ecf29e1f0e1a274b72c020d93b34a4e
MattHazard <mrm.mue@gmail.com>
2021-10-07 14:49:18 -0700

    runGUI script uses matlab r2021a and does not load a python module

--- runGUI.sh
+++ runGUI.sh
@@ -1,6 +1,6 @@
 module load gcc/8.3.0
-module load python/3.7
-module load matlab/r2020b
+#module load python/3.7
+module load matlab/r2021a
 module load cuda/10.2
 module load cudnn/7.6.5
 module load Qt/5.15.2
