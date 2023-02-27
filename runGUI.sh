8419fabdadf784b3b72905ff88bcb20b55ec6b79
Matthew Mueller <mrm.mue@gmail.com>
2023-02-27 12:45:00 -0800

    Delete runGUI.sh

--- runGUI.sh
+++
@@ -1,7 +0,0 @@
-module load gcc/8.3.0
-#module load python/3.7
-module load matlab/r2021a
-module load cuda/10.2
-module load cudnn/7.6.5
-module load Qt/5.15.2
-/clusterfs/fiona/matthewmueller/LLSM_Processing_GUI/AppRun
